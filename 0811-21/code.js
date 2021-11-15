61586b3cb0d8d8c1a47b643b01e917736b247abc
kristaiida <92725476+kristaiida@users.noreply.github.com>
2021-11-15 09:54:49 +0200

    Tehtävät tehty

--- 0811-21/code.js
+++ 0811-21/code.js
@@ -26,7 +26,36 @@ for(let i = 0; i < 6; i++) {
 }
 
 // e
+let table = document.createElement("table")
+let tr = [document.createElement("tr"), document.createElement("tr")]
+let th = [document.createElement("th"), document.createElement("th")]
+let td = [document.createElement("td"), document.createElement("td")]
+th[0].textContent = "Last name"
+th[1].textContent = "First name"
+td[0].textContent = "John"
+td[1].textContent = "Doe"
+
+tr[0].append(th[0],th[1])
+tr[1].append(td[0],td[1])
+
+table.append(tr[0], tr[1])
+
+content.appendChild(table)
 
 // f
+let elems = document.querySelectorAll("#content>*")
+
+for (const e of elems) {
+   let hr = document.createElement("hr")
+   content.insertBefore(hr, e);
+}
 
 // g
+elems = document.querySelectorAll("div>p")
+
+for (const e of elems) {
+   console.log(e);
+   let span = document.createElement("span")
+   span.textContent = "***"
+   e.parentElement.insertBefore(span, e);
+}
