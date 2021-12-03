3744741cdb140c3335eca14720a509a63123fb09
kristaiida <92725476+kristaiida@users.noreply.github.com>
2021-12-03 15:01:30 +0200

    gitignore

--- 3011-21/code.js
+++
@@ -1,7 +0,0 @@
-let phones;
-
-fetch('https://api-mobilespecs.azharimm.site/v2/brands/honor-phones-121')
-        .then(response => response.json())
-        .then(rest_data => phones=rest_data.data.phones)
-        .catch(e => console.log(e));
-
